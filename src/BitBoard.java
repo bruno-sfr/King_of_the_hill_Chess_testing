ade6908e2e39b2ab7444cc59822bc453b0c25ccf
Bruno Schaffer <brunoschaffer@eduroam-141-23-143-230.wlan.tu-berlin.de>
2023-04-25 10:41:21 +0200

    Initial commit The very basic of a Bitboard

---
+++ src/BitBoard.java
@@ -0,0 +1,51 @@
+public class BitBoard {
+    private long board;
+
+    public BitBoard() {
+        board = 0L;
+    }
+
+    public BitBoard(long pboard) {
+        board = pboard;
+    }
+
+    public void setSquare(int square) {
+        board |= (1L << square);
+    }
+
+    public void clearSquare(int square) {
+        board &= ~(1L << square);
+    }
+
+    public boolean isSquareSet(int square) {
+        return ((board & (1L << square)) != 0);
+    }
+
+    public long getBoard() {
+        return board;
+    }
+
+    public void setBoard(long board) {
+        this.board = board;
+    }
+
+    public String boardWithChar(char c){
+        String binaryString = Long.toBinaryString(board);
+        String _formattedString = String.format("%64s", binaryString).replace('1', c);
+        return String.format("%64s", _formattedString).replace(' ', '0');
+    }
+
+    public void printBoard() {
+        String binaryString = Long.toBinaryString(board);
+        String formattedString = String.format("%64s", binaryString).replace(' ', '0');
+        for (int rank = 7; rank >= 0; rank--) {
+            for (int file = 0; file < 8; file++) {
+                int square = rank * 8 + file;
+                System.out.print(formattedString.charAt(Math.abs(63-square)) + " ");
+            }
+            System.out.println("|" + (char)('A' + rank));
+        }
+        System.out.println("_______________");
+        System.out.println("1 2 3 4 5 6 7 8");
+    }
+}
---
+++ src/Chessboard.java
@@ -0,0 +1,124 @@
+import java.lang.reflect.Array;
+
+public class Chessboard {
+    BitBoard Black;
+    BitBoard White;
+    BitBoard Pawn;
+    BitBoard Rook;
+    BitBoard King;
+    BitBoard Queen;
+    BitBoard Knight;
+    BitBoard Bishop;
+
+    public Chessboard(){
+        Black = new BitBoard(0xffff000000000000L);
+        White = new BitBoard(0x000000000000ffffL);
+        Pawn = new BitBoard(0x00ff00000000ff00L);
+        Rook = new BitBoard(0x8100000000000081L);
+        King = new BitBoard(0x1000000000000010L);
+        Queen = new BitBoard(0x0800000000000008L);
+        Bishop = new BitBoard(0x2400000000000024L);
+        Knight = new BitBoard(0x4200000000000042L);
+    }
+
+    public void printBoard(){
+        /*
+        System.out.println("Black");
+        Black.printBoard();
+        System.out.println("White");
+        White.printBoard();
+        System.out.println("Pawn");
+        Pawn.printBoard();
+        System.out.println("Rook");
+        Rook.printBoard();
+        System.out.println("King");
+        King.printBoard();
+        System.out.println("Queen");
+        Queen.printBoard();
+        System.out.println("Knight");
+        Knight.printBoard();
+        System.out.println("Bishop");
+        Bishop.printBoard();
+        */
+
+        char[] Humanboard = new char[64];
+
+        //Black
+        BitBoard black_pawns = new BitBoard(Black.getBoard() & Pawn.getBoard());
+        BitBoard black_king = new BitBoard(Black.getBoard() & King.getBoard());
+        BitBoard black_queen = new BitBoard(Black.getBoard() & Queen.getBoard());
+        BitBoard black_knights = new BitBoard(Black.getBoard() & Knight.getBoard());
+        BitBoard black_bishop = new BitBoard(Black.getBoard() & Bishop.getBoard());
+        BitBoard black_rook = new BitBoard(Black.getBoard() & Rook.getBoard());
+        String _black_pawns = black_pawns.boardWithChar('p');
+        String _black_king = black_king.boardWithChar('k');
+        String _black_queen = black_queen.boardWithChar('q');
+        String _black_knights = black_knights.boardWithChar('n');
+        String _black_bishop = black_bishop.boardWithChar('b');
+        String _black_rook = black_rook.boardWithChar('r');
+        BitBoard white_pawns = new BitBoard(White.getBoard() & Pawn.getBoard());
+        BitBoard white_king = new BitBoard(White.getBoard() & King.getBoard());
+        BitBoard white_queen = new BitBoard(White.getBoard() & Queen.getBoard());
+        BitBoard white_knights = new BitBoard(White.getBoard() & Knight.getBoard());
+        BitBoard white_bishop = new BitBoard(White.getBoard() & Bishop.getBoard());
+        BitBoard white_rook = new BitBoard(White.getBoard() & Rook.getBoard());
+        String _white_pawns = white_pawns.boardWithChar('P');
+        String _white_king = white_king.boardWithChar('K');
+        String _white_queen = white_queen.boardWithChar('Q');
+        String _white_knights = white_knights.boardWithChar('N');
+        String _white_bishop = white_bishop.boardWithChar('B');
+        String _white_rook = white_rook.boardWithChar('R');
+        for(int i = 0;i<64;i++){
+            if(_black_pawns.charAt(i)!='0'){
+                Humanboard[i] = _black_pawns.charAt(i);
+            }
+            else if(_black_bishop.charAt(i)!='0'){
+                Humanboard[i] = _black_bishop.charAt(i);
+            }
+            else if(_black_king.charAt(i)!='0'){
+                Humanboard[i] = _black_king.charAt(i);
+            }
+            else if(_black_knights.charAt(i)!='0'){
+                Humanboard[i] = _black_knights.charAt(i);
+            }
+            else if(_black_queen.charAt(i)!='0'){
+                Humanboard[i] = _black_queen.charAt(i);
+            }
+            else if(_black_rook.charAt(i)!='0'){
+                Humanboard[i] = _black_rook.charAt(i);
+            }
+            else if(_white_pawns.charAt(i)!='0'){
+                Humanboard[i] = _white_pawns.charAt(i);
+            }
+            else if(_white_bishop.charAt(i)!='0'){
+                Humanboard[i] = _white_bishop.charAt(i);
+            }
+            else if(_white_king.charAt(i)!='0'){
+                Humanboard[i] = _white_king.charAt(i);
+            }
+            else if(_white_knights.charAt(i)!='0'){
+                Humanboard[i] = _white_knights.charAt(i);
+            }
+            else if(_white_queen.charAt(i)!='0'){
+                Humanboard[i] = _white_queen.charAt(i);
+            }
+            else if(_white_rook.charAt(i)!='0'){
+                Humanboard[i] = _white_rook.charAt(i);
+            }
+            else{
+                Humanboard[i] = '0';
+            }
+        }
+
+        for (int rank = 7; rank >= 0; rank--) {
+            for (int file = 0; file < 8; file++) {
+                int square = rank * 8 + file;
+                System.out.print(Humanboard[63-square] + " ");
+            }
+            System.out.println("|" + (char)('A' + rank));
+        }
+        System.out.println("_______________");
+        System.out.println("1 2 3 4 5 6 7 8");
+
+    }
+}
